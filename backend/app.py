2f65073ca1c8e66ed0d40fae38071ac431b444b8
Walt Downing <waltdowning@outlook.com>
2025-10-12 15:03:51 -0700

    Filling out the initial files with barebones, copied content.

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+# Copied from https://dev.to/yahiaqous/how-to-build-a-crud-api-using-python-flask-and-sqlalchemy-orm-with-postgresql-2jjj
+
+import os
+
+# App Initialization
+from . import create_app # from __init__ file
+app = create_app(os.getenv("CONFIG_MODE"))
+
+# Hello World!
+@app.route('/')
+def hello():
+      return "Hello World!"
+
+if __name__ == "__main__":
+      app.run()
