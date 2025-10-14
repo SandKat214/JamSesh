b0cfc06f2d7679ef960caa33ae07f9814e7f58f2
SandKat214 <sandeenk@oregonstate.edu>
2025-10-13 22:38:27 -0700

    Adds AuthContext and hooks for tracking authenticated user and logging them out

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,35 @@
 import { Box, Typography } from "@mui/material"
 import { createBrowserRouter } from "react-router"
 import { RouterProvider } from "react-router/dom"
+import { useMemo } from "react"
+
+// hooks
+import { useAuth } from "./context/auth/useAuth"
 
 // pages
 import Login from "./pages/Login"
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: (
-			<Box>
-				<Typography color='jamRed'>Home Page</Typography>
-			</Box>
-		),
-	},
-	{
-		path: "login",
-		Component: Login,
-	},
-])
-
 export default function App() {
+	const { user } = useAuth()
+
+	// TODO: Add Navigation to redirect depending on user authentication
+	// Consider making a ProtectedRoute component as an alternative to useMemo
+	const router = useMemo(() => {
+		return createBrowserRouter([
+			{
+				path: "/",
+				element: (
+					<Box>
+						<Typography color='jamRed'>Home Page</Typography>
+					</Box>
+				),
+			},
+			{
+				path: "login",
+				Component: Login,
+			},
+		])
+	}, [user])
+
 	return <RouterProvider router={router} />
 }
