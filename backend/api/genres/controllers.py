8f8edcdc38e0d89943f62feb179c1f3dc9b9e779
vmkiss <veronika.kiss17@gmail.com>
2025-10-15 12:55:47 -0700

    unpack json into variables

--- backend/api/genres/controllers.py
+++ backend/api/genres/controllers.py
@@ -1,8 +1,18 @@
 from flask import request
 
+# Register controller logic based on this YouTube tutorial: https://www.youtube.com/watch?v=mjZIv4ey0ps&list=PL4cUxeGkcC9g8OhpOZxNdhXggFz2lOuCT&index=3
 def register_controller():
-    user_info = request.get_json()
-    pass
+    new_user = request.get_json()
+
+    # unpack json into variables
+    username = new_user.get('username')
+    email = new_user.get('email')
+    password = new_user.get('password')
+    profile_pic = new_user.get('profile_pic')
+    bio = new_user.get('bio')
+    city = new_user['city']
+    state = new_user['state']
+
 
 def login_controller():
     pass
