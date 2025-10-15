6c80181f89b126b5ba9e83f7b51f0d5bb9f4e5a7
vmkiss <veronika.kiss17@gmail.com>
2025-10-15 11:59:19 -0700

    initialize JWT

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,7 @@
 # Copied from https://dev.to/yahiaqous/how-to-build-a-crud-api-using-python-flask-and-sqlalchemy-orm-with-postgresql-2jjj
 
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -8,6 +9,7 @@ from .config import config
 
 db = SQLAlchemy()
 migrate = Migrate()
+jwt = JWTManager()
 
 def create_app(config_mode):
       app = Flask(__name__)
@@ -15,9 +17,9 @@ def create_app(config_mode):
 
       db.init_app(app)
       migrate.init_app(app, db)
+      jwt.init_app(app)
 
       # Reflect the database tables after initializing the app
-      # NOTE: Check to make sure this will not cause circular dependencies!
       with app.app_context():
             db.reflect()
 
