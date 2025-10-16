c59065bdf8c2053fca87b59225dc623308cf9905
Walt Downing <waltdowning@outlook.com>
2025-10-16 13:24:24 -0700

    Removing test function to add instruments

--- backend/api/instruments/controllers.py
+++ backend/api/instruments/controllers.py
@@ -1,6 +1,4 @@
-from flask import request, jsonify
-
-from ... import db
+from flask import jsonify
 from ...models import Instrument
 
 
@@ -8,27 +6,3 @@ def list_all_instruments_controller():
     instruments = Instrument.query.all()
     instruments_list = [instrument.toDict() for instrument in instruments]  # Using the toDict method
     return jsonify(instruments_list)
-
-
-# Current app design will not require the ability to add instruments via api.
-# This is for testing purposes.
-def add_instrument_controller():
-    # Retrieve the data elements and put them in a dictionary
-    instrument_to_add = request.json
-
-    # Create a database object to put the data elements in
-    new_instrument = Instrument()
-
-    # Add the data elements to the new database object
-    for key, value in instrument_to_add.items():
-        if hasattr(new_instrument, key):
-            setattr(new_instrument, key, value)
-
-    # Add and commit to database
-    db.session.add(new_instrument)
-    db.session.commit()
-
-    return jsonify({
-            "message": "Instrument added",
-            "instrument": new_instrument.toDict()
-        }), 201
--- backend/api/instruments/urls.py
+++ backend/api/instruments/urls.py
@@ -1,12 +1,10 @@
 from flask import request
 
 from ...app import app
-from .controllers import list_all_instruments_controller, add_instrument_controller
+from .controllers import list_all_instruments_controller
 
 
-@app.route("/instruments", methods=['GET', 'POST'])
+@app.route("/instruments", methods=['GET'])
 def instruments():
     if request.method == 'GET':
         return list_all_instruments_controller()
-    if request.method == 'POST':
-        return add_instrument_controller()
