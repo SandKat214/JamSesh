59758210a953d3d95d9c3623a95c49b8997acfd1
alexgulyan16 <gulyana@oregonstate.edu>
2025-10-16 18:04:35 -0600

    Add background, logo, and box to home/login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,9 +19,43 @@ export default function App() {
 			{
 				path: "/",
 				element: (
-					<Box>
-						<Typography color='jamRed'>Home Page</Typography>
-					</Box>
+                    // Page background
+                    <Box sx={{
+                    width: "100vw",
+                    height: "100vh",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "top",
+                    paddingTop: "4%",
+                    paddingBottom: "0px",
+                    alignItems: "center",
+                    backgroundImage: "url(/background.png)",
+                    backgroundPosition: "bottom",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundAttachment: "fixed",
+                    }}>
+                        <img src="/JamSesh_tagline.svg" width="350px" height="135px"></img>
+
+                        {/* Login box, blur effect adapted from https://css.glass */}
+                        <Box sx={{
+                        width: "600px",
+                        height: "620px",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        marginTop: "4%",
+                        paddingTop: "2em",
+                        borderRadius: "4em",
+                        background: "rgba(255, 255, 255, 0.58)",
+                        boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+                        backdropFilter: "blur(10.1px)",
+                        webkitBackdropFilter: "blur(10.1px)",
+                        border: "1px solid rgba(255, 255, 255, 0.08)",
+                        }}>
+                            <Typography color='purple.dark' fontSize='3em'>Welcome Back</Typography>
+                        </Box>
+                    </Box>
 				),
 			},
 			{
