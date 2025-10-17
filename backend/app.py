1ff595f2b4fabfec83b5275cd2a834c24087e7df
vmkiss <veronika.kiss17@gmail.com>
2025-10-16 17:51:32 -0700

    enable CORS handling

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,12 @@
 # Copied from https://dev.to/yahiaqous/how-to-build-a-crud-api-using-python-flask-and-sqlalchemy-orm-with-postgresql-2jjj
 
 import os
+from flask_cors import CORS
 
 # App Initialization
 from . import create_app  # from __init__ file
 app = create_app(os.getenv("CONFIG_MODE"))
-
+cors = CORS(app, origins="http://127.0.0.1:5173")
 
 # Hello World!
 @app.route('/')
